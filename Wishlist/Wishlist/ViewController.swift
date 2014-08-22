5654b4905251a3da96455dfe0922700ff25d3c3e
Ede <hedavid@QV020265.local>
2014-08-22 13:21:59 +0200

    ProfileViewController aufgebaut

--- Wishlist/Wishlist/ViewController.swift
+++ Wishlist/Wishlist/ViewController.swift
@@ -9,10 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    var welcomeLabel: UILabel!
+    var infoLabel: UILabel!
+    var chooseButton: UIButton!
+    var newButton: UIButton!
+    
+    // MARK: - Meine Methoden
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Welcome-Text
+        welcomeLabel = UILabel (frame: CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 40))
+        welcomeLabel.text = "Willkommen"
+        welcomeLabel.font = UIFont(name: "Chalkduster", size: 16)
+        welcomeLabel.backgroundColor = UIColor.clearColor()
+        welcomeLabel.textAlignment = .Center
+        self.view.addSubview(welcomeLabel)
+        
+        // Info Text
+        infoLabel = UILabel (frame: CGRectMake(0, CGRectGetMaxY(welcomeLabel.frame), CGRectGetWidth(self.view.frame), 120))
+        infoLabel.text = "wähle deinen Charakter aus oder erstelle einen neuen!"
+        infoLabel.font = UIFont(name: "Chalkduster", size: 14)
+        infoLabel.backgroundColor = UIColor.clearColor()
+        infoLabel.textAlignment = .Center
+        infoLabel.numberOfLines = 0
+        self.view.addSubview(infoLabel)
+        
+        // Choose Button
+        chooseButton = UIButton (frame: CGRectMake(40, CGRectGetMaxY(infoLabel.frame) + 20, CGRectGetWidth(self.view.frame) - 80, 40))
+        chooseButton.setTitle("Peter", forState: UIControlState.Normal)
+        chooseButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        chooseButton.setTitleColor(UIColor.orangeColor(), forState: UIControlState.Highlighted)
+        chooseButton.layer.borderWidth = 1.0
+        chooseButton.backgroundColor = UIColor.whiteColor()
+        chooseButton.layer.cornerRadius = 15
+        self.view.addSubview(chooseButton)
+    
+        // New Button
+        newButton = UIButton (frame: CGRectMake(40, CGRectGetMaxY(chooseButton.frame) + 20, CGRectGetWidth(self.view.frame) - 80, 40))
+        newButton.setTitle("Neuer Character", forState: UIControlState.Normal)
+        newButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        newButton.setTitleColor(UIColor.orangeColor(), forState: UIControlState.Highlighted)
+        newButton.layer.borderWidth = 1.0
+        newButton.backgroundColor = UIColor.whiteColor()
+        newButton.layer.cornerRadius = 15
+        self.view.addSubview(newButton)
+        newButton.addTarget(self, action: "newButtonTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +66,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prefersStatusBarHidden() -> Bool {
+        // Versteckt die Statusbar
+        return true
+    }
 
+    override func viewWillLayoutSubviews() {
+        welcomeLabel.frame = CGRectMake(CGRectGetMinX(welcomeLabel.frame), CGRectGetMinY(welcomeLabel.frame), CGRectGetWidth(self.view.frame), CGRectGetHeight(welcomeLabel.frame))
+    }
+    
+    // MARK: - Actions
+    func newButtonTapped(sender: UIButton!) {
+        println("Button Tapped")
+        let newProfileViewController = NewProfileViewController()
+        self.showViewController(newProfileViewController, sender: nil)
+    }
 }
 
---
+++ Wishlist/Wishlist/ViewController/NewProfileViewController.swift
@@ -0,0 +1,80 @@
+//
+//  NewProfileViewController.swift
+//  Wishlist
+//
+//  Created by Henning David on 22.08.14.
+//  Copyright (c) 2014 SwiftFighters. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var pictureImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.modalTransitionStyle = UIModalTransitionStyle.PartialCurl
+        self.setupScreen()
+    }
+    
+    func setupScreen() {
+        var y: CGFloat = 0
+        var yMargin: CGFloat = 15
+        
+        // Info Text
+        var infoLabel = UILabel (frame: CGRectMake(0, y, CGRectGetWidth(self.view.frame), 120))
+        infoLabel.text = "erstelle hier dein neues Profil"
+        infoLabel.font = UIFont(name: "Chalkduster", size: 14)
+        infoLabel.backgroundColor = UIColor.clearColor()
+        infoLabel.textAlignment = .Center
+        infoLabel.numberOfLines = 0
+        self.view.addSubview(infoLabel)
+        
+        y += CGRectGetHeight(infoLabel.frame) + yMargin
+        
+        // Picture Button
+        var pictureButton = UIButton(frame: CGRectMake(10, y, CGRectGetWidth(self.view.frame) / 2, 40))
+        pictureButton.setTitle("Bild auswählen", forState: UIControlState.Normal)
+        pictureButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        pictureButton.setTitleColor(UIColor.orangeColor(), forState: UIControlState.Highlighted)
+        pictureButton.layer.borderWidth = 1.0
+        pictureButton.layer.cornerRadius = 15
+        self.view.addSubview(pictureButton)
+        pictureButton.addTarget(self, action: "capture:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // Picture Image View
+        pictureImageView = UIImageView(frame: CGRectMake(CGRectGetMaxX(pictureButton.frame), y, CGRectGetWidth(self.view.frame) / 2 - 30, 80))
+        pictureImageView.backgroundColor = UIColor.greenColor()
+        self.view.addSubview(pictureImageView)
+        
+
+
+        
+    }
+    
+    // MARK: - Actions
+    func capture(sender: UIButton) {
+        // Bild auswählen
+        var imgViewController = UIImagePickerController()
+        imgViewController.delegate = self
+        imgViewController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(imgViewController, animated: true, completion: nil)
+    }
+    
+    
+    
+    // MARK: - UIImagePicker Delegate
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!) {
+        println("Have an image")
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        //pictureImageView.
+        
+    }
+
+
+
+}
