36e1b07011b789aa5018e6aa8a55d4e1ef67aa76
Ede <hedavid@qv020265.fritz.box>
2014-08-22 21:50:17 +0200

    Name: Label/Input Alter: Label

--- Wishlist/Wishlist/ViewController/NewProfileViewController.swift
+++ Wishlist/Wishlist/ViewController/NewProfileViewController.swift
@@ -22,6 +22,8 @@ class NewProfileViewController: UIViewController, UIImagePickerControllerDelegat
     func setupScreen() {
         var y: CGFloat = 0
         var yMargin: CGFloat = 15
+        var leftMargin: CGFloat = 10
+        var rightMargin: CGFloat = 20
         
         // Info Text
         var infoLabel = UILabel (frame: CGRectMake(0, y, CGRectGetWidth(self.view.frame), 120))
@@ -35,15 +37,43 @@ class NewProfileViewController: UIViewController, UIImagePickerControllerDelegat
         y += CGRectGetHeight(infoLabel.frame) + yMargin
         
         // Picture Button
-        var picButtonFrame: CGRect = CGRectMake(10, y+20, CGRectGetWidth(self.view.frame) / 2, 40)
-        var pictureButton = UiUtil.createButton("Bild auswählen", myFrame: picButtonFrame)
+        var picButtonFrame: CGRect = CGRectMake(yMargin, y+20, CGRectGetWidth(self.view.frame) / 2, 40)
+        var pictureButton = UiUtil.createButton("wähle ein Bild aus", myFrame: picButtonFrame)
         self.view.addSubview(pictureButton)
         pictureButton.addTarget(self, action: "capture:", forControlEvents: UIControlEvents.TouchUpInside)
         
         // Picture Image View
-        pictureImageView = UIImageView(frame: CGRectMake(CGRectGetMaxX(pictureButton.frame) + yMargin, y, CGRectGetWidth(self.view.frame) / 2 - 30, 80))
+        pictureImageView = UIImageView(frame: CGRectMake(CGRectGetMaxX(pictureButton.frame) + yMargin, y, CGRectGetWidth(self.view.frame) / 2 - rightMargin, 80))
         pictureImageView.backgroundColor = UIColor.greenColor()
         self.view.addSubview(pictureImageView)
+        
+        y += CGRectGetHeight(pictureImageView.frame) + yMargin
+        
+        // Name Label
+        var nameLabel = UILabel (frame: CGRectMake(0, y, CGRectGetWidth(self.view.frame) - rightMargin, 40))
+        nameLabel.text = "Wie heißt du?"
+        nameLabel.font = UIFont(name: "Chalkduster", size: 14)
+        nameLabel.backgroundColor = UIColor.clearColor()
+        nameLabel.textAlignment = .Center
+        self.view.addSubview(nameLabel)
+        
+        y += CGRectGetHeight(nameLabel.frame)
+        
+        // Name Input
+        var nameInput = UITextField (frame: CGRectMake(10, y, CGRectGetWidth(self.view.frame) - rightMargin, 40))
+        nameInput.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(nameInput)
+        
+        y += CGRectGetHeight(nameLabel.frame) + yMargin
+
+        // Age Label
+        var ageLabel = UILabel (frame: CGRectMake(0, y, CGRectGetWidth(self.view.frame) - rightMargin, 40))
+        ageLabel.text = "Wie alt bist du?"
+        ageLabel.font = UIFont(name: "Chalkduster", size: 14)
+        ageLabel.backgroundColor = UIColor.clearColor()
+        ageLabel.textAlignment = .Center
+        self.view.addSubview(ageLabel)
+
     }
     
     
